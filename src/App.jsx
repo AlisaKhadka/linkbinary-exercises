b0411d51233c74a2ac856e5580b9923c73a18b34
AlisaKhadka <alisakhadka2@gmail.com>
2025-12-15 15:12:46 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ProfileCard from "./components/ProfileCard"
+function App() {
+
+  return (
+    <div>
+      <ProfileCard
+        name="Alisa Khadka"
+        title="Software Engineering Intern"
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,10 @@
+ import React from "react"
+ export default function ProfileCard({ name, title }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
