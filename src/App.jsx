8b5f2fb4a0ceb297715d4b7105fce70ebfe7c12e
AlisaKhadka <alisakhadka2@gmail.com>
2025-12-16 15:47:50 +0545

    Add SimpleCounter with state and event handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import ProfileCard from "./components/ProfileCard"
+import ProfileCard from "./components/ProfileCard";
+import SimpleCounter from "./components/SimpleCounter";
 function App() {
 
   return (
     <div>
+    
       <ProfileCard
         name="Alisa Khadka"
         title="Software Engineering Intern"
         address="mechinagar-07"
         email="alisaakhadka@gmail.com"
       />
+
+      <SimpleCounter/>
+
     </div>
   )
 }
